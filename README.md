f50db6eac6ebb46f241c9e6dae937cb10237eecb
Fateh Farooqui <farooquifateh184@gmail.com>
2022-06-04 12:46:36 +0530

    v0.0.1

---
+++ README.md
@@ -0,0 +1,100 @@
+# rn-media-query-hook
+
+[![npm version](https://img.shields.io/npm/v/rn-media-query-hook.svg?style=for-the-badge)](https://www.npmjs.com/package/rn-media-query-hook)
+[![npm downloads](https://img.shields.io/npm/dm/rn-media-query-hook.svg?style=for-the-badge)](https://www.npmjs.com/package/rn-media-query-hook)
+[![npm](https://img.shields.io/npm/dt/rn-media-query-hook.svg?style=for-the-badge)](https://www.npmjs.com/package/rn-media-query-hook)
+[![npm](https://img.shields.io/npm/l/rn-media-query-hook?style=for-the-badge)](https://github.com/fateh999/rn-media-query-hook/blob/master/LICENSE)
+
+React Native Minimalistic UI Library with barebones components to easily create any UI you want
+
+#### Web Demo :
+
+https://snack.expo.dev/@fateh999/rn-media-query-hook
+
+#### Steps to install :
+
+```javascript
+
+npm install rn-media-query-hook
+
+```
+
+or
+
+```javascript
+
+yarn add rn-media-query-hook
+
+```
+
+```javascript
+import useMediaQuery from "rn-media-query-hook";
+
+// In functional component
+const { mediaQuery, mediaQueryLogic } = useMediaQuery();
+```
+
+or
+
+```javascript
+import useMediaQuery from "rn-media-query-hook";
+
+// In functional component
+const { mQ, mQL } = useMediaQuery();
+```
+
+#### Usage :
+
+```javascript
+// For dimensions
+const responsiveWidth = mQ([100, 200, 300, 400, 500]);
+
+// order is important [base, sm, md, lg, xl]
+
+// or
+
+const responsiveWidth = mQ({ base: 100, sm: 200, md: 300, lg: 400, xl: 500 });
+
+// For any logic
+const show = mQL([true, true, true, false, false]);
+
+// order is important [base, sm, md, lg, xl]
+
+// or
+
+const show = mQ({ base: true, sm: true, md: true, lg: false, xl: false });
+```
+
+### Default Breakpoints :
+
+```javascript
+const initialBreakPoints: BREAK_POINTS = {
+  base: 0,
+  sm: 480,
+  md: 768,
+  lg: 992,
+  xl: 1280,
+};
+```
+
+### Custom Breakpoints :
+
+```javascript
+const newBreakPoints: BREAK_POINTS = {
+  base: 0,
+  sm: 380,
+  md: 868,
+  lg: 892,
+  xl: 1180,
+};
+
+// In functional component
+
+const { mediaQuery, mediaQueryLogic } = useMediaQuery(newBreakPoints);
+```
+
+### Note :
+
+- Only base dimension or logic is required, rest are optional
+
+<p><a href="https://www.buymeacoffee.com/fateh999"> <img align="left" src="https://cdn.buymeacoffee.com/buttons/v2/default-yellow.png" height="50" width="210" alt="fateh999" /></a></p><br><br><br>
---
+++ src/useMediaQuery.ts
@@ -0,0 +1,110 @@
+import { useCallback, useEffect, useState } from "react";
+import { useWindowDimensions } from "react-native";
+
+type BREAK_POINTS = {
+  base: number;
+  sm: number;
+  md: number;
+  lg: number;
+  xl: number;
+};
+
+type BREAK_POINTS_LOGIC = {
+  base: any;
+  sm: any;
+  md: any;
+  lg: any;
+  xl: any;
+};
+
+const initialBreakPoints: BREAK_POINTS = {
+  base: 0,
+  sm: 480,
+  md: 768,
+  lg: 992,
+  xl: 1280,
+};
+
+function useMediaQuery(customBreakPoints?: BREAK_POINTS) {
+  const { width } = useWindowDimensions();
+  const [breakPoints, setBreakPoints] = useState(initialBreakPoints);
+
+  useEffect(() => {
+    if (customBreakPoints) {
+      setBreakPoints(customBreakPoints);
+    }
+  }, [customBreakPoints]);
+
+  const resultantDimension = useCallback(
+    (base: number, sm?: number, md?: number, lg?: number, xl?: number) => {
+      if (width > breakPoints.base && width <= breakPoints.sm) {
+        return base;
+      } else if (width > breakPoints.sm && width <= breakPoints.md) {
+        return sm ?? base;
+      } else if (width > breakPoints.md && width <= breakPoints.lg) {
+        return md ?? sm ?? base;
+      } else if (width > breakPoints.lg && width <= breakPoints.xl) {
+        return lg ?? md ?? sm ?? base;
+      } else {
+        return xl ?? lg ?? md ?? sm ?? base;
+      }
+    },
+    [width, breakPoints]
+  );
+
+  const resultantValue = useCallback(
+    (base: any, sm: any, md: any, lg: any, xl: any) => {
+      if (width > breakPoints.base && width <= breakPoints.sm) {
+        return base;
+      } else if (width > breakPoints.sm && width <= breakPoints.md) {
+        return sm ?? base;
+      } else if (width > breakPoints.md && width <= breakPoints.lg) {
+        return md ?? sm ?? base;
+      } else if (width > breakPoints.lg && width <= breakPoints.xl) {
+        return lg ?? md ?? sm ?? base;
+      } else {
+        return xl ?? lg ?? md ?? sm ?? base;
+      }
+    },
+    [width, breakPoints]
+  );
+
+  const mediaQuery = useCallback(
+    (
+      dimensions: Array<number> | (Partial<BREAK_POINTS> & { base: number })
+    ) => {
+      if (Array.isArray(dimensions)) {
+        const [base, sm, md, lg, xl] = dimensions;
+        return resultantDimension(base, sm, md, lg, xl);
+      } else {
+        const { base, sm, md, lg, xl } = dimensions;
+        return resultantDimension(base, sm, md, lg, xl);
+      }
+    },
+    [resultantDimension]
+  );
+
+  const mediaQueryLogic = useCallback(
+    (
+      dimensions: Array<any> | (Partial<BREAK_POINTS_LOGIC> & { base: any })
+    ) => {
+      if (Array.isArray(dimensions)) {
+        const [base, sm, md, lg, xl] = dimensions;
+        return resultantValue(base, sm, md, lg, xl);
+      } else {
+        const { base, sm, md, lg, xl } = dimensions;
+        return resultantValue(base, sm, md, lg, xl);
+      }
+    },
+    [resultantValue]
+  );
+
+  return {
+    mediaQuery,
+    mediaQueryLogic,
+    mQ: mediaQuery,
+    mQL: mediaQueryLogic,
+  };
+}
+
+export default useMediaQuery;
